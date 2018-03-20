581eb34c6d3d32f099c9e4517e7c635f29e871e8
haoziapple <haozixiaowang@163.com>
2018-03-20 17:40:10 +0800

    update : add yo ng project

---
+++ yoNgApp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoNgAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoNgAppApp
+ */
+angular.module('yoNgAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
