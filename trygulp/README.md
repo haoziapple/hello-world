c49e1ac55d99010f8c1fa43978a16204f8f48c52
haoziapple <haozixiaowang@163.com>
2018-05-02 16:47:07 +0800

    add some code to try gulp

--- trygulp/README.md
+++ trygulp/README.md
@@ -7,3 +7,4 @@
 3. 编写gulpfile.js
 4. Run gulp:
 > gulp
+
--- trygulp/gulpfile.js
+++ trygulp/gulpfile.js
@@ -3,3 +3,10 @@ var gulp = require('gulp');
 gulp.task('default', function() {
 	
 });
+
+gulp.task('try', function () {
+	
+	// 不return的话会多出来一个output文件夹
+	return gulp.src('./*.js')
+		.pipe(gulp.dest('build'));
+});
