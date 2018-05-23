f9eadcf665444dc8b618b058fb7f0f1706505333
haoziapple <haozixiaowang@163.com>
2018-05-23 19:52:25 +0800

    尝试抓取省一级编码，www.stats.gov.cn好像挂掉了。。

--- py-blockchain/district/district.py
+++ py-blockchain/district/district.py
@@ -16,27 +16,33 @@ def StringListSave(save_path, filename, slist):
 
 def Page_Info(myPage):
     '''Regex'''
-    # mypage_Info = re.findall(r'<div class="titleBar" id=".*?"><h2>(.*?)</h2><div class="more"><a href="(.*?)">.*?</a></div></div>', myPage, re.S)
-    mypage_Info = re.findall(r'<td><a href=\'(.*?)\'>(.*?)<br/></a></td>', myPage, re.S)
+    mypage_Info = re.findall(r'<td><a href=\'(.*?)\.html\'>(.*?)<br/></a></td>', myPage, re.S)
     return mypage_Info
 
+def ProvincePage_Info(myPage):
+    '''Regex'''
+    mypage_Info = re.findall(r'<tr class="citytr"><td><a href=\'.*?\.html\'>.*?</a></td><td><a href=\'(.*?)\.html\'>(.*?)</a></td></tr>', myPage, re.S)
+    return mypage_Info
 
 def Spider(baseUrl):
-    i = 0
     print "downloading ", baseUrl
     headers = {'user-agent':'PostmanRuntime/7.1.1'}
     myPage = requests.get(baseUrl,headers=headers).content.decode("ISO-8859-1")
     myPageResults = Page_Info(myPage)
 
     save_path = u"地区抓取"
-    filename = str(i)+"_"+u"地区"
+    filename = u"地区"
     StringListSave(save_path, filename, myPageResults)
 
-    i += 1
 
-    for url, item in myPageResults:
-        print "downloading ", url.encode("ISO-8859-1")
+    for code, item in myPageResults:
         print u"省级单位 ", item.encode("ISO-8859-1")
+        provincePage = requests.get(baseUrl + code.encode("ISO-8859-1") + '.html',headers=headers).content.decode("ISO-8859-1")
+        provinceResults = ProvincePage_Info(provincePage)
+
+        filename=u"省级单位"
+        StringListSave(save_path, filename, provinceResults)
+
 
 if __name__ == '__main__':
     print "start"
